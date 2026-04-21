2cd1dce42d929469eb5b5e32bceb2bbd3eb38cce
AndreaUnige <andrea.sciarrone@unige.it>
2026-04-21 16:47:41 +0200

    Refactoring

--- Android/MoviesRecognizer/app/src/main/java/com/andrea/moviessuggestor/network/MyHttpRequest.java
+++ Android/MoviesRecognizer/app/src/main/java/com/andrea/moviessuggestor/network/MyHttpRequest.java
@@ -11,6 +11,7 @@ import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -59,7 +60,10 @@ public class MyHttpRequest {
                 error -> {
                     Toast.makeText(context, error.toString(), Toast.LENGTH_SHORT).show();
                     Log.i(TAG, "onErrorResponse: " + error.toString());
-                }) {
+                })
+
+        // Anonymous class
+        {
 
             @Override
             protected Response<String> parseNetworkResponse(NetworkResponse response) {
